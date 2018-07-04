71720b297ef68d7d510be51edc9472dbbffef709
Bodo Kaiser <i@bodokaiser.io>
2018-07-04 16:45:53 +0200

    improved memory management

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <ostream>
 #include <stdexcept>
+#include <utility>
 
 template <typename T>
 size_t num_digits(T number) {
@@ -14,32 +15,33 @@ size_t num_digits(T number) {
   return std::ceil(std::log10(number));
 }
 
+using dim_t = std::pair<size_t, size_t>;
+
 template <typename T>
 class Matrix {
  public:
   Matrix() : Matrix(0, 0){};
-  Matrix(const Matrix& matrix) : Matrix(matrix.rows_, matrix.cols_) {
+  Matrix(const Matrix& matrix) : Matrix(matrix.rows(), matrix.cols()) {
     std::copy(matrix.data_, matrix.data_ + size(), data_);
   }
   Matrix(Matrix&& matrix) noexcept : data_(std::move(matrix.data_)) {
-    rows_ = matrix.rows_;
-    cols_ = matrix.cols_;
+    dim_ = matrix.dim_;
     matrix.data_ = nullptr;
   }
-  Matrix(size_t m, size_t n) noexcept
-      : rows_(m), cols_(n), data_(new T[m * n]) {
+  Matrix(size_t m, size_t n) noexcept : dim_(m, n), data_(new T[m * n]) {
     fill(0);
   }
   ~Matrix() noexcept { delete[] data_; }
 
-  size_t size() const { return rows_ * cols_; }
-  size_t rows() const { return rows_; }
-  size_t cols() const { return cols_; }
+  dim_t dim() const { return dim_; };
+  size_t size() const { return rows() * cols(); }
+  size_t rows() const { return dim().first; }
+  size_t cols() const { return dim().second; }
 
   void fill(T value) {
-    for (size_t i = 0; i < cols_; i++) {
-      for (size_t j = 0; j < rows_; j++) {
-        data_[i * rows_ + j] = value;
+    for (size_t i = 0; i < cols(); i++) {
+      for (size_t j = 0; j < rows(); j++) {
+        data_[i * rows() + j] = value;
       }
     }
   }
@@ -56,41 +58,43 @@ class Matrix {
     if (this == &matrix) {
       return *this;
     }
-    delete[] data_;
+    if (data_ != nullptr) {
+      delete[] data_;
+    }
     data_ = matrix.data_;
-    rows_ = matrix.rows_;
-    cols_ = matrix.cols_;
+    dim_ = matrix.dim_;
     matrix.data_ = nullptr;
+    matrix.dim_ = dim_t(0, 0);
 
     return *this;
   }
 
   T& at(size_t i, size_t j) {
-    if (i >= rows_ || j >= cols_) {
+    if (i >= rows() || j >= cols()) {
       throw std::out_of_range("index out of range");
     }
 
-    return data_[i * rows_ + j];
+    return data_[i * rows() + j];
   }
 
   T at(size_t i, size_t j) const {
-    if (i >= rows_ || j >= cols_) {
+    if (i >= rows() || j >= cols()) {
       throw std::out_of_range("index out of range");
     }
 
-    return data_[i * rows_ + j];
+    return data_[i * rows() + j];
   }
 
   friend std::ostream& operator<<(std::ostream& stream,
                                   const Matrix<T>& matrix) {
-    for (size_t i = 0; i < matrix.cols_; i++) {
-      for (size_t j = 0; j < matrix.rows_; j++) {
-        T element = matrix.data_[i * matrix.rows_ + j];
+    for (size_t i = 0; i < matrix.cols(); i++) {
+      for (size_t j = 0; j < matrix.rows(); j++) {
+        T element = matrix.data_[i * matrix.rows() + j];
 
         stream << std::left << std::setw(4) << std::setfill(' ') << element;
       }
 
-      if (i != matrix.cols_ - 1) {
+      if (i != matrix.cols() - 1) {
         stream << std::endl;
       }
     }
@@ -99,7 +103,6 @@ class Matrix {
   }
 
  private:
-  size_t rows_ = 0;
-  size_t cols_ = 0;
+  dim_t dim_;
   T* data_;
 };
